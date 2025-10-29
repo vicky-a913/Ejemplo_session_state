4775476211246aab76569fc453091248fa747afa
Victoria Izquierdo <a385983@uach.mx>
2025-10-29 09:17:25 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,7 @@ if 'count' not in st.session_state:
 if st.button('Click me'):
   st.session_state['count'] += 1
 
+nombre = st.text_input("Escribe tu nombre")
+st.write(nombre)
+
 st.write(st.session_state)
