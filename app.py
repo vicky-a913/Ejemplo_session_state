25dab949ec7a265134f4db3a143c4766464ee71d
Victoria Izquierdo <a385983@uach.mx>
2025-10-29 09:20:04 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ st. title("Ejemplo para usar session_state")
 if 'count' not in st.session_state:
   st.session_state['count'] = 0
 
+if 'name' not in st.session_state:
+  st.session_state['name'] = ''
+
 if st.button('Click me'):
   st.session_state['count'] += 1
 
