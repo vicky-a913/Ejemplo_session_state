ad3936a4f01d6ac85d6076cf344bcdb84004f523
Victoria Izquierdo <a385983@uach.mx>
2025-10-29 09:33:58 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ if 'count' not in st.session_state:
   st.session_state['count'] = 0
 
 if 'name' not in st.session_state:
-  st.session_state['name'] = 'n'
+  st.session_state['name'] = ''
 
 if st.button('Click me'):
   st.session_state['count'] += 1
